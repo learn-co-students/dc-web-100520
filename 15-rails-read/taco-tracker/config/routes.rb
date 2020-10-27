2d97073a167e1ec8299f59b0b8461b4347027aeb
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-10-27 12:18:13 -0400

    made index and show pages

--- 15-rails-read/taco-tracker/config/routes.rb
+++ 15-rails-read/taco-tracker/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/", to: "application#home"
-  get "/hello/:name", to: "application#hello"
+  get "/hello/:name", to: "application#hello", as: "hello"
   get "/addition/:x/:y", to: "application#adding", as: "adding"
   get "/about_yummy_foods", to: "application#about", as: "about_tacos"
+  get "/tacos", to: "tacos#index"
+  get "/tacos/:id", to: "tacos#show", as: "taco"
 end
