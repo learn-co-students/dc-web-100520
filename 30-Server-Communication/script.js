e35191a7b5ec130da08713520e5fb618fdbd2090
Adam Johnson <adamwjo@gmail.com>
2020-11-18 19:01:28 -0500

    pushing up tomorrows code

---
+++ 30-Server-Communication/script.js
@@ -0,0 +1,40 @@
+console.log("we in");
+
+const pokeContainer = () => document.querySelector('#pokemon-container')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch('http://localhost:3000/pokemon')
+        .then(res => res.json())
+        .then(pokemons => {pokemons.forEach( pokemon => renderPokemon(pokemon))
+        })
+
+})
+
+const renderPokemon = (pokemon) => {
+    let gridItem = document.createElement('div')
+        gridItem.classList.add('col-sm-6')
+
+    let card = document.createElement('div')
+        card.classList.add('card', 'm-3', 'w-25')
+
+    let cardImg = document.createElement('img')
+        cardImg.src = pokemon.sprite
+        cardImg.classList.add('card-img-top')
+    
+    let cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+    let cardTitle = document.createElement('h5')
+        cardTitle.classList.add('card-title')
+        cardTitle.innerText = pokemon.name
+        cardBody.appendChild(cardTitle)
+
+    
+    card.append(cardImg, cardBody)
+
+    gridItem.appendChild(card)
+    
+    pokeContainer().appendChild(card)
+
+}
